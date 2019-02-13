1bfab2531c0c09084ac31e1b93781c75b9176e34
sakupo <sugiyama.s.ah@m.titech.ac.jp>
2019-02-14 02:52:10 +0900

    Remove 'ontouchstart'

--- src/components/topmenu/About.vue
+++ src/components/topmenu/About.vue
@@ -2,7 +2,7 @@
 <template lang="pug">
 #aboutmain
   .titlelink
-    router-link(to="/about/" ontouchstart="")
+    router-link(to="/about/")
       h1 {{ page_title }}
     h2 My portfolio website.
   table(v-if="display")
