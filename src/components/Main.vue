e91d8280c66ff66bd4c9f21d34f9bed7a806443a
sakupo <sugiyama.s.ah@m.titech.ac.jp>
2020-04-09 06:11:55 +0900

    add animation

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,35 +2,35 @@
 #main
   .container
     .row
-      .col
+      .col(class="item1")
         router-link(to="/about")
           .item
             img(src="../assets/user.png" width="120px" height="120px")
             h4 About
       .col
         a(href="https://github.com/sakupo")
-          .item
+          .item(class="item2")
             img(src="../assets/GitHub-Mark-120px-plus.png" width="120px" height="120px")
             h4 GitHub
       .col
         a(href="https://twitter.com/_saku3")
-          .item
+          .item(class="item3")
             img(src="../assets/twitterLogo.png" width="120px" height="120px")
             h4 Twitter
     .row
       .col
         router-link(to="/dashboard")
-          .item
+          .item(class="item4")
             img(src="../assets/schedule.png" width="120px" height="120px")
             h4 Schedule 
       .col
         a(href="https://sakupo.github.io/Diary/")
-          .item
+          .item(class="item5")
             img(src="../assets/diary.png" width="120px" height="120px")
             h4 Diary
       .col
         a(href="https://cpucno.hatenablog.com/archive")
-          .item
+          .item(class="item6")
             img(src="../assets/hatenablog-logo.eps" width="120px" height="120px")
             h4 Blog
 </template>
@@ -74,23 +74,6 @@ export default {
 .col
   padding: 60px
   margin: 0 auto
-$itemcolor: #899eeb
-$shadow-width: 10px
-$shadow-width-hover: 2px
-.item
-  display: inline-block
-  width: 160px
-  height: 160px
-  border-radius: 50%
-  text-decoration: none
-  text-align: center
-  background: white
-  box-shadow:0 $shadow-width 0 $shadow-width $itemcolor
-  transform: translateY(-$shadow-width)
-  transition:0.3s all
-  &:hover
-    box-shadow:0 $shadow-width-hover 0 0 $itemcolor
-    transform: translateY(-$shadow-width-hover)
 
 h1, h2
   font-weight: normal
@@ -109,5 +92,32 @@ a
       color: #000
     &:hover
       color: #000
- 
+@for $i from 1 through 6
+  .item#{$i}
+    animation: dropIn#{$i} 2s ease 0s 1 normal
+    -webkit-animation: dropIn#{$i} 2s ease 0s 1 normal
+  @keyframes dropIn#{$i}
+    0%
+      opacity: 0
+    #{$i}0%
+      transform: translate(0, 8vh)
+    100%
+      opacity: 1
+$itemcolor: #899eeb
+$shadow-width: 10px
+$shadow-width-hover: 2px
+.item
+  display: inline-block
+  width: 160px
+  height: 160px
+  border-radius: 50%
+  text-decoration: none
+  text-align: center
+  background: white
+  box-shadow:0 $shadow-width 0 $shadow-width $itemcolor
+  transform: translateY(-$shadow-width)
+  transition:0.3s all
+  &:hover
+    box-shadow:0 $shadow-width-hover 0 0 $itemcolor
+    transform: translateY(-$shadow-width-hover)
 </style>
